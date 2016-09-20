98807aab93c22109b156be6d3d4f8828c86f402d
Dani <daniel.soler@e-campus.uab.cat>
2016-09-20 11:57:21 +0200

    init

---
+++ __init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+##############
+# OpenMM: Light interface to run
+# MD and explore whether or not a structure is stable.
+# Authors:  Daniel Soler Viladrich
+#            <daniel.soler@e-campus.uab.cat>
+#          
+# Web: https://bitbucket.org/
+##############
